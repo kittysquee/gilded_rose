69d86bafe78ee90a47244e51e9985bf157fd8827
kittysquee <kittysquee@gmail.com>
2017-05-17 10:22:26 +0100

    Adds list of things I wish to test for

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,46 +15,39 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != BRIE and item.name != BACKSTAGE_PASS
-        if item.quality > MINIMUM_QUALITY
-          if item.name != SULFURAS
-            item.quality = item.quality - ITEM_INCREMENT_VALUE
-          end
+      if item.name != BRIE && item.name != BACKSTAGE_PASS
+        if item.quality > MINIMUM_QUALITY && item.name != SULFURAS
+          item.quality -= ITEM_INCREMENT_VALUE
         end
       else
         if item.quality < MAXIMUM_QUALITY
-          item.quality = item.quality + ITEM_INCREMENT_VALUE
+          item.quality += ITEM_INCREMENT_VALUE
           if item.name == BACKSTAGE_PASS
             if item.sell_in < 11
-              if item.quality < MAXIMUM_QUALITY
-                item.quality = item.quality + ITEM_INCREMENT_VALUE
+              if item.quality < MAXIMUM_QUALITY && item.quality += ITEM_INCREMENT_VALUE
               end
             end
             if item.sell_in < 6
-              if item.quality < MAXIMUM_QUALITY
-                item.quality = item.quality + ITEM_INCREMENT_VALUE
+              if item.quality < MAXIMUM_QUALITY && item.quality += ITEM_INCREMENT_VALUE
               end
             end
           end
         end
       end
-      if item.name != SULFURAS
-        item.sell_in = item.sell_in - ITEM_INCREMENT_VALUE
+      if item.name != SULFURAS && item.sell_in -= ITEM_INCREMENT_VALUE
       end
       if item.sell_in < 0
         if item.name != BRIE
           if item.name != BACKSTAGE_PASS
             if item.quality > MINIMUM_QUALITY
-              if item.name != SULFURAS
-                item.quality = item.quality - ITEM_INCREMENT_VALUE
+              if item.name != SULFURAS && item.quality -= ITEM_INCREMENT_VALUE
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality -= item.quality
           end
         else
-          if item.quality < MAXIMUM_QUALITY
-            item.quality = item.quality + ITEM_INCREMENT_VALUE
+          if item.quality < MAXIMUM_QUALITY && item.quality += ITEM_INCREMENT_VALUE
           end
         end
       end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,6 +2,8 @@ require 'gilded_rose'
 
 describe GildedRose do
 
+  subject (:gilded_rose) { described_class.new }
+
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("fixme", 0, 0)]
@@ -9,8 +11,68 @@ describe GildedRose do
       expect(items[0].name).to eq "fixme"
     end
 
-    it "should have a maximum quality" do
-      items = 
+    # it "should have a maximum quality" do
+    #   items = [Item.new("Aged Brie, 0, 50")]
+    #   gilded_rose(items).update_quality()
+    #   expect(items[0].quality::MAXIMUM_QUALITY).to eq 50
+    # end
+
+    context 'sulfuras' do
+      it 'never has to be sold (sell in of 0)' do
+
+      end
+
+      it 'never decreases in quality' do
+
+      end
+
+      it 'has a quality of 80' do
+
+      end
+    end
+
+    context 'aged brie' do
+      it 'increases in quality as it gets older' do
+
+      end
+
+      it 'never has a quality over 50' do
+
+      end
+
+      it 'once sell in is 0, it\'s quality drops twice as quickly' do
+
+      end
+    end
+
+    context 'backstage passes' do
+      it 'increases in value as time passes' do
+
+      end
+
+      it 'when sellin is 10 or less, quality increases by 2' do
+
+      end
+
+      it 'when sellin is 5 or less, quality increases by 3' do
+
+      end
+
+      it 'when sellin is 0, quality is 0' do
+
+      end
+    end
+
+    context 'conjured items' do
+      it 'decreases in quality twice as quick (by 2 daily)' do
+
+      end
+    end
+
+    context 'sell in' do
+      it 'decreases by 1 every day' do
+        
+      end
     end
   end
 end
